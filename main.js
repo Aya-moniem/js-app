fe02a2eaca3137190869bfd08734f31707745f9c
Aya Abd Elmoniem <ayamonm401@gmail.com>
2024-06-30 20:32:40 +0300

    mouse as fire app using js

---
+++ main.js
@@ -0,0 +1,12 @@
+var myImage = document.querySelector('img');
+
+
+document.addEventListener('mousemove' ,function (e) {
+    myImage.style.left = e.clientX + 'px';
+    myImage.style.top = e.clientY + 'px';
+  
+})
+
+
+
+
